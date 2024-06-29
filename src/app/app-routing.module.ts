0b685110551dca11646b2f8a1ba20ca6ced2b27f
Abdulla K A <abdulla.kaliparambil@gmail.com>
2024-06-29 15:23:25 +0530

    angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
+  {path:"", component:LoginComponent},
+  {path:"dashboard", component:DashboardComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
